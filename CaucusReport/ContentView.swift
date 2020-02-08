853867a65ce2f7747024de7c0f068a209699a745
Darrell Root <darrellrootjunk@mac.com>
2020-02-08 09:46:53 -0800

    Initial Commit

---
+++ CaucusReport/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CaucusReport
+//
+//  Created by Darrell Root on 2/8/20.
+//  Copyright © 2020 net.networkmom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
